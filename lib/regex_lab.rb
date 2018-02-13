a7425153eeca1a7b64e1f6084f896b9e23912e58
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-13 01:47:27 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,10 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match (/^[A-Z].+[\.!?]$/) ? true : false
+if text.match (/^[A-Z].+[\.!?]$/)
+  return true
+else
+  false
 end
 
 def valid_phone_number?(phone)
